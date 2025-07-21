8475bed70d4f68b94d4178f6eb0a6f7c6b7b4b6a
Hearin <154088592+Hearin1@users.noreply.github.com>
2025-07-20 18:32:47 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+/*
+Commenting out instead of deleting file. 
+
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. Add the close symbol here.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+*/
