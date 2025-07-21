e26074afc8e0e0c5fe0d9ea9e3cf0763070a49fa
Hearin <154088592+Hearin1@users.noreply.github.com>
2025-07-20 18:25:49 -0600

    Create users.js

---
+++ app_api/routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const ctrlUsers = require('../controllers/users');
+
+router.post('/register', ctrlUsers.register);
+router.post('/login', ctrlUsers.login);
+router.put('/:username/settings', ctrlUsers.updateSettings);
+
+module.exports = router;
