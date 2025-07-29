fea2d246b374406b3758ca10e74d916595a9e805
Hearin <154088592+Hearin1@users.noreply.github.com>
2025-07-28 22:23:30 -0600

    Update users.js

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   hashedPassword: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
   email: String,
   settings: {
     preferredResort: String,
