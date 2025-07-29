01bcd1210ed117f2e6a54d51c9aa8ba17ba74a14
Hearin <154088592+Hearin1@users.noreply.github.com>
2025-07-28 22:23:58 -0600

    Update users.js

--- app_api/routes/users.js
+++ app_api/routes/users.js
@@ -5,5 +5,6 @@ const ctrlUsers = require('../controllers/users');
 router.post('/register', ctrlUsers.register);
 router.post('/login', ctrlUsers.login);
 router.put('/:username/settings', ctrlUsers.updateSettings);
+router.get('/:username/dashboard', ctrlUsers.dashboard);
 
 module.exports = router;
