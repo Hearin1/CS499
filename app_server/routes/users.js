50b5b44f3f9a85f88e6bfa8328baacbda380cf47
Hearin <154088592+Hearin1@users.noreply.github.com>
2025-07-28 22:25:03 -0600

    Update users.js

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var dashboardCtrl = require('../controllers/dashboard');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/dashboard/:username', dashboardCtrl.dashboard);
+
 
 module.exports = router;
